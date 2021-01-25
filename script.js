2e3da3822864ff2d0d41f2b468a5206b8742c79b
alexdermenji <allexdermenji@gmail.com>
2021-01-25 22:22:44 +0000

    smooth scroll for nav menu links

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -31,21 +33,16 @@ document.addEventListener('keydown', function (e) {
 });
 
 ////// Smooth scrolling (old-style)
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-console.log(btnScrollTo);
-const section1 = document.querySelector('#section--1');
-console.log(section1);
+btnScrollTo.addEventListener('click', function () {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+//   // Smooth scrolling (new-style)
 
-btnScrollTo.addEventListener('click', function (e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
 
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: 'smooth',
-  });
-
-  ////// Smooth scrolling (new-style)
-  // section1.scrollIntoView({ behavior: 'smooth' });
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
