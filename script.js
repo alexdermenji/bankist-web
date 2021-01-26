f9177f3b623da82e9be05a33dacbf529b9426d63
alexdermenji <allexdermenji@gmail.com>
2021-01-26 17:49:49 +0000

    nav menu hover effect for links

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+///////////////////////////////////////Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,12 +35,12 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// Smooth scrolling (new-style)
+///////////////////// Smooth scrolling (new-style)
 btnScrollTo.addEventListener('click', function () {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-// Nav menu scrolling
+////////////////////// Nav menu scrolling
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -47,10 +50,7 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
-// Tabbed components
-const tabs = document.querySelectorAll('.operations__tab');
-const tabContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
+/////////////////// Tabbed components
 
 tabContainer.addEventListener('click', function (e) {
   e.preventDefault();
@@ -58,17 +58,44 @@ tabContainer.addEventListener('click', function (e) {
 
   //active tab
   if (!clicked) return;
+  //remove acyive class for tabs
   tabs.forEach(t => {
     t.classList.remove('operations__tab--active');
   });
-
+  //remove active content area
   tabsContent.forEach(t => {
     t.classList.remove('operations__content--active');
   });
+  //add active class for tab
   clicked.classList.add('operations__tab--active');
-  //activate content area
 
+  //add active class for content
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+///////////////////////////menu fade animation
+
+//function opacity hover effect
+const handleHover = function (e) {
+  console.log(this);
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    //choose all nav_links in parent element .nav
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    //choose all images in .nav
+    const logo = link.closest('.nav').querySelector('img');
+
+    //change opacity fo links
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    //change opacity for logo
+    logo.style.opacity = this;
+  }
+};
+//add event listener for link
+console.log(nav);
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+// nav.addEventListener('mouseout', handleHover.bind(1));
