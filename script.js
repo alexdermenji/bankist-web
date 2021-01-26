fe4ddbf08e515857416025613f37d2c4beb1c7d7
alexdermenji <allexdermenji@gmail.com>
2021-01-26 22:18:11 +0000

    Reveal sections with IntersectionObserver

--- script.js
+++ script.js
@@ -122,3 +122,23 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+////////////////////////Reveal sections with IntersectionObserver
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobsorve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
